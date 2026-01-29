4226f2a713b6e0a62d0633e5646d840bd4bcac82
Jair Hilário <2347049@kiit.ac.in>
2026-01-29 17:27:41 +0530

    remove src, use dist only

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("user"); // ou useContext/AuthProvider
-
-  return isAuthenticated ? children : <Navigate to="/Login" />;
-};
-
-export default PrivateRoute;
